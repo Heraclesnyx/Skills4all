a176f1a4a05a2f17ef68038bcb51f98c16c2160d
Thomas <anthony1.reboul@eptech.eu>
2023-07-07 00:30:13 +0200

    Modification de l'affichage de la température grace à l'aide du Kernel et modification de la fixture afin d'intégré les catégory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Car;
+use App\Entity\Category;
 use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,14 +14,31 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+        // Create 10 categories.
+        for ($i=0; $i < 10 ; $i++) {
+                $category = new Category();
+                $category->setName($faker->name);
+                $manager->persist($category);
+            }
+        
+            $manager->flush();
+        
+        /** @var Category[] $categories */
+        $categories = $manager->getRepository(Category::class)->findAll();
+
         //create 100 voitures
         for ($i=0; $i < 100 ; $i++) { 
             $car = new Car();
-            $car->setName($faker->name);
-            $car->setPrice($faker->mt_rand(10, 11));
+
+            $car
+                ->setName($faker->name)
+                ->setPrice($faker->randomNumber(3))
+                ->setCategory($categories[mt_rand(0,9)]);
+            
             $manager->persist($car);
         }
         
         $manager->flush();
+
     }
 }
